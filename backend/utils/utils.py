320fba5cb348facf73ade06397078af297b24a9f
dofenghost <dofenspot@gmail.com>
2025-10-09 17:57:23 +0700

    New "Generate User Tag" Method

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -1,5 +1,3 @@
-
-
 def generate_name() -> str:
     from random import randint
     names = ["Жираф", "Енот", "Медведь", "Собака", "Цыпленок",
@@ -10,3 +8,12 @@ def generate_name() -> str:
         "Маляр", "Биолог", "Физик", "Охранник", "Директор"]
     
     return f"{names[randint(0, len(names)-1)]}-{jobs[randint(0, len(jobs)-1)]} {randint(0, 100)}"
+
+
+def generate_user_tag() -> str:
+    from random import randint
+    tag_number: int = randint(1, 0xFFFFFF)
+    return "@{:06x}".format(tag_number)
+
+if __name__ == "__main__":
+    print(generate_user_tag())
